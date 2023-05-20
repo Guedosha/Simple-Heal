54df44f0932404c2c3e7e0e88315e1bcd93e4925
Guedosha <guedosha@guedosha.com>
2023-05-20 12:29:29 -0600

    Hotfix
    Hotfix to fix the plugin listing the incorrect plugin version in the console upon launch

--- src/main/java/com/guedosha/simpleheal/HealCommand.java
+++ src/main/java/com/guedosha/simpleheal/HealCommand.java
@@ -14,41 +14,43 @@ public class HealCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        //checks if the sender is a player or not
         if (sender instanceof Player p) {
+            if (!p.hasPermission("Simpleheal.heal")) {
+                p.sendMessage(ChatColor.RED + "You don't have permission to use that command");
+                return true;
+            }
+
             if (args.length == 0) {
-                p.sendMessage("You have been healed");
+                p.sendMessage(ChatColor.GOLD + "You have been healed");
                 p.setHealth(p.getMaxHealth());
             } else if (args.length == 1) {
                 String playerName = args[0];
                 Player target = Bukkit.getServer().getPlayerExact(playerName);
                 if (target == null) {
-                    p.sendMessage("That Player Isn't Online");
+                    p.sendMessage(ChatColor.RED + "That player isn't online");
                 } else {
-                    p.sendMessage(ChatColor.RED + target.getName() + ChatColor.GOLD + " has been healed");
+                    p.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + target.getName() + ChatColor.GOLD + " has been healed");
                     target.sendMessage(ChatColor.GOLD + "You have been healed");
                     target.setHealth(target.getMaxHealth());
                 }
             } else {
-                p.sendMessage("Usage: /heal ; /heal <target>");
+                p.sendMessage(ChatColor.RED + "Usage: /heal or /heal <target>");
             }
-        //else, meaning if the sender isn't a player IE the console or a command block
         } else {
             if (args.length == 0) {
-                plugin.getLogger().info("Usage: /heal <target>");
+                plugin.getLogger().info(ChatColor.RED + "Usage: /heal <target>");
             } else if (args.length == 1) {
                 String playerNameConsole = args[0];
                 Player t = Bukkit.getServer().getPlayerExact(playerNameConsole);
-
                 if (t == null) {
-                    plugin.getLogger().info("That Player is not Online");
+                    plugin.getLogger().info(ChatColor.RED + "That player isn't online");
                 } else {
                     t.setHealth(20);
                     t.sendMessage(ChatColor.GOLD + "You have been healed");
-                    plugin.getLogger().info(t.getName() + " Has been healed");
+                    plugin.getLogger().info(ChatColor.YELLOW + "" + ChatColor.BOLD + t.getName() + ChatColor.GOLD + " has been healed");
                 }
             } else {
-                plugin.getLogger().info("Usage: /heal <target>");
+                plugin.getLogger().info(ChatColor.RED + "Usage: /heal <target>");
             }
         }
         return true;
